9e4983a50d5a5080f48f91067697c62e0e97b24d
G-U-T <147336533+G-U-T@users.noreply.github.com>
2024-01-11 14:23:45 -0500

    Styled login component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import '../output.css';
 
 const Login = () => {
   return (
-    <div>
-      <h1>User Log-in Form</h1>
-      <form>
-        <label>
+    <div className='bg-black rounded-xl p-3 drop-shadow-lg border-2 border-whitecream'>
+      <form className='flex flex-col gap-3'>
+        <h1 className='text-xl text-whitecream font-serif italic'>User Log-in Form</h1>
+        <label className='flex flex-row justify-center gap-3 text-gray'>
             Username:
-            <input type="text" name="name"/>
+            <input type="text" name="name"
+            className='rounded-md bg-verydarkgray text-whitecream'/>
         </label>
-        <br/>
-        <br/>
-        <label>
+        <label className='flex flex-row justify-center gap-3 text-gray'>
             Password:
-            <input type="password" name="password"/><br/>
+            <input type="password" name="password"
+            className='rounded-md bg-verydarkgray text-whitecream' /><br/>
         </label>
       </form>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      'black': '#0a091a',
+      'white': '#fffff2',
+      'whitecream': '#fff4e8',
+      'verylightgray': '#dce6e5',
+      'lightgray': '#b6c3cc',
+      'gray': '#8894a8',
+      'darkgray': '#444566',
+      'verydarkgray': '#2f2b45',
+      'lightpink': '#ffd4de',
+      'superpink': '#fb82ff',
+      'purple': '#783c78',
+      'lightyellow': '#ffffa3',
+      'yellow': '#ffe082',
+      'brown': '#ab5b55',
+      'tan': '#b0918f',
+      'lightgreen': '#b4ff82',
+      'green': '#6ce680',
+      'lightblue': '#bfffee',
+      'blue': '#82deff',
+      'orange': '#ffa480',
+    }
   },
   plugins: [],
 }
