e9cc54bcf5de4fafe425cdadd84ec07c03f8d9e4
justice jennings <justice.l.jennings@gmail.com>
2024-01-10 23:31:41 -0500

    added seed data to seed file.

---
+++ seed.js
@@ -0,0 +1,307 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// Perform database seeding operations using prisma
+
+const seedDb = () {
+  // Example seeding logic for User_Preferences and Outfit models
+  const UserPreferences1 = await prisma.UserPreferences.create({
+    data: {
+      username:     'mo_mo',
+      password:     'pword1',
+      email:        'momo3@gmail.com',
+      occasion:     'casual',
+      style_type:   'gothic',
+      forMen:       false,
+      color:        'black'
+    },
+  });
+
+  const UserPreferences2 = await prisma.UserPreferences.create({
+    data: {
+      username:     'tony8',
+      password:     'pword2',
+      email:        'tonytoni@gmail.com',
+      occasion:     'formal',
+      style_type:   'elegance',
+      forMen:        true,
+      color:        'pink'
+    },
+  });
+
+  const UserPreferences3 = await prisma.UserPreferences.create({
+    data: {
+      username:     'ali_2003',
+      password:     'alice62',
+      email:        'alicejones@gmail.com',
+      occasion:     'business',
+      style_type:   'business',
+      forMen:        false,
+      color:        'pink'
+    },
+  });
+
+  const Outfit = await prisma.Outfit.create({
+    data: {
+      user_id: userPreferences.id,
+      name:                     'goth fit',
+      occasion:                 'casual',
+      style_type:               'gothic',
+      forMen:                   'no',
+      color:                    'black',
+      weather_compatability:    'winter'     
+    },
+  });
+
+  const Clothing_Item1 = await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'sporty',
+      forMen:                    true,
+      garment_type:             'shorts',
+      color:                    'tan',
+      weather_compatability:    'summer',
+      img_url:                  'https://imgur.com/uNB6Yu5.png',
+      description:              'Tan cargo shorts.'
+    },
+  });
+
+  const Clothing_Item2 = await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'athleisure',
+      forMen:                    true,
+      garment_type:             'pants',
+      color:                    'gray',
+      weather_compatability:    'winter',
+      img_url:                  'https://imgur.com/kthDcLz.png',
+      description:              'Gray nike trackpants.'
+    },
+  });
+
+  const Clothing_Item3 = await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'business',
+      style_type:               'business',
+      forMen:                    true,
+      garment_type:             'trousers',
+      color:                    'tan',
+      weather_compatability:    'summer',
+      img_url:                  'https://imgur.com/CigfvuO.png',
+      description:              'tan chino-trouser pants.'
+    },
+  });
+
+  const Clothing_Item4 = await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'streetwear',
+      forMen:                    true,
+      garment_type:             'jacket',
+      color:                    'blue',
+      weather_compatability:    'winter',
+      img_url:                  'https://imgur.com/zGZwsdb.png',
+      description:              'Blue puffer jacket.'
+    },
+  });
+
+  const Clothing_Item5 = await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'streetwear',
+      forMen:                    true,
+      garment_type:             'pants',
+      color:                    'blue',
+      weather_compatability:    'winter',
+      img_url:                  'https://imgur.com/Sc7lx48.png',
+      description:              'multipocket cargo blue-jeans.'
+    },
+  });
+
+  const Clothing_Item6 = await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'business',
+      style_type:               'business',
+      forMen:                    false,
+      garment_type:             'trousers',
+      color:                    'charcoal',
+      weather_compatability:    'winter',
+      img_url:                  'https://imgur.com/q7jSZCC',
+      description:              'charcoal gray workforce trousers'
+    },
+  });
+
+  const Clothing_Item7 = await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'athleisure',
+      forMen:                    false,
+      garment_type:             'leggings',
+      color:                    'muave',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/scurolo.png',
+      description:              'muave leggings.'
+    },
+  });
+
+  const Clothing_Item8 = await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'athleisure',
+      forMen:                    false,
+      garment_type:             'sweatpants',
+      color:                    'light-gray',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/R1Tdhk5.png',
+      description:              'light-gray tapered sweatpants.'
+    },
+  });
+
+  const Clothing_Item9= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'grunge',
+      forMen:                    false,
+      garment_type:             'pants',
+      color:                    'light-gray',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/766ozjW.png',
+      description:              'Ripped blue jeans.'
+    },
+  });
+
+  const Clothing_Item10= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'elegance',
+      forMen:                    false,
+      garment_type:             'blouse',
+      color:                    'light-gray',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/wUxT9Qu.png',
+      description:              'tie-waste, rouched sleeve blouse.'
+    },
+  });
+
+  const Clothing_Item11= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'elegance',
+      forMen:                    false,
+      garment_type:             'blouse',
+      color:                    'off-white',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/wUxT9Qu.png',
+      description:              '3/4 sleeve, tie-neck blouse.'
+    },
+  });
+
+
+  const Clothing_Item12= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'elegance',
+      forMen:                    false,
+      garment_type:             'blouse',
+      color:                    'black',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/qnSsVNZ.png',
+      description:              'tiered, long-sleeve blouse.'
+    },
+  });
+
+  const Clothing_Item13= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'chic',
+      forMen:                    false,
+      garment_type:             'blouse',
+      color:                    'white',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/aSIepwn.png',
+      description:              'white and black cropped polka dot blouse.'
+    },
+  });
+
+  const Clothing_Item14= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'casual',
+      forMen:                    true,
+      garment_type:             'shirt',
+      color:                    'red',
+      weather_compatability:    'summer',
+      img_url:                  'https://imgur.com/YOWksxX.png',
+      description:              'red polo shirt.'
+    },
+  });
+
+  const Clothing_Item15= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'casual',
+      forMen:                    true,
+      garment_type:             'shirt',
+      color:                    'green',
+      weather_compatability:    'summer',
+      img_url:                  'https://imgur.com/YOWksxX.png',
+      description:              'green crewneck t-shirt.'
+    },
+  });
+
+  const Clothing_Item16= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'casual',
+      forMen:                    true,
+      garment_type:             'shirt',
+      color:                    'blue',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/Cv4qT8B.png',
+      description:              'blue plaid flannel shirt.'
+    },
+  });
+
+  const Clothing_Item17= await prisma.Clothing_Item.create({
+    data: {
+      user_id: userPreferences.id,
+      occasion:                 'casual',
+      style_type:               'casual',
+      forMen:                    true,
+      garment_type:             'shirt',
+      color:                    'red',
+      weather_compatability:    'fall',
+      img_url:                  'https://imgur.com/Cv4qT8B.png',
+      description:              'red plaid flannel shirt.'
+    },
+  });
+
+  console.log('Database seeded successfully.');
+}
+
+// Call the seeding function
+seedDatabase()
+  .catch((error) => console.error(error))
+  .finally(async () => {
+    // Disconnect the Prisma client after seeding
+    await prisma.$disconnect();
+  });
+
+await prisma.$disconnect();
