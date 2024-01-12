769547eecce475d64554d53fb18353591d26b4d2
G-U-T <147336533+G-U-T@users.noreply.github.com>
2024-01-12 16:31:09 -0500

    Can POST to register route

--- server.cjs
+++ server.cjs
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const loginRoute = require('./api/login.cjs');
+const registerRoute = require('./api/register.cjs');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-
-app.post('/login', loginRoute)
+app.use('/login', loginRoute)
+app.use('/register', registerRoute)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
