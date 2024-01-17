01e95e896f8cfc72b421c0c8eb30a110ae81a4a6
G-U-T <147336533+G-U-T@users.noreply.github.com>
2024-01-17 12:20:45 -0500

    Front-end registration works, and logging in correctly compares to hashed passwords.

--- api/login.cjs
+++ api/login.cjs
@@ -1,23 +1,37 @@
 const express = require('express');
 const router = express.Router();
+
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+const bcrypt = require('./bcrypt.cjs');
 
 router.post('/', async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const user = await prisma.user.findFirst({
+    // Find the user with the same username.
+    const user = await prisma.user.findUnique({
       where: {
-        username: username,
-        password: password,
+        username,
       },
     });
 
     if (user) {
-      res.json({ success: true, message: 'Login successful' });
+
+      // Compare the entered password to the user's hashed password:
+      const passwordsMatch = await bcrypt.compare(password, user.password);
+      
+      if (passwordsMatch) {
+        // TODO: Will probably need to give the user their token here.
+
+        res.json({ success: true, message: 'Login successful!' });
+      } else {
+        res.status(401).json({ success: false, message: 'Invalid credentials' });
+      }
+
     } else {
-      res.status(401).json({ success: false, message: 'Invalid credentials' });
+      res.status(401).json({ success: false, message: 'No user of that username exists.' });
     }
   } catch (error) {
     console.error('Error during login:', error);
