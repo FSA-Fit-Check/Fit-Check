18a4187fedd9ed2bd2f1c5d3e5560b4632982a6d
justice jennings <justice.l.jennings@gmail.com>
2024-01-18 12:47:42 -0500

    re-migrated prisma schema to accomodate changes made within schema.

---
+++ prisma/migrations/20240118030503_removed_img_url_column_from_outfit_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `img_url` on the `Outfit` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Outfit" DROP COLUMN "img_url";
