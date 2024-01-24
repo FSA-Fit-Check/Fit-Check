f2363044bada273b620e7791b3061fa3acaec827
ChrisNieves <nievescs20@gmail.com>
2024-01-24 11:49:14 -0500

    fixed conflict garmentUploadForm

--- src/components/garmentUpload.jsx
+++ src/components/garmentUpload.jsx
@@ -18,134 +18,13 @@ const GarmentUploadForm = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/garment_upload', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3000/garment_upload", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formInput),
       });
-<<<<<<< HEAD
-=======
-    
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        // try {
-        //     const response = await fetch('http://localhost:3000/userprefform', {
-        //       method: 'POST',
-        //       headers: {
-        //         'Content-Type': 'application/json',
-        //       },
-        //       body: JSON.stringify(formInput),
-        //     });
-      
-        //     const result = await response.json();
-      
-        //     if (result.success) {
-        //       setSearchResults(result.data)
-        //       console.log('Preferences submitted successfully:', result.data);
-        //     } else {
-        //       console.error('Error submitting preferences:', result.error);
-        //     }
-        //   } catch (error) {
-        //     console.error('Error during form submission:', error);
-        //   }};
-        // console.log("Form submitted:", formInput);
-      };
-    
-  return (
-        <form className='panel' onSubmit={handleSubmit}>
-          <label>
-            Garment Type:
-            <select 
-              name="Garment Type"
-              value={formInput.garmentType}
-              onChange={(e) => setFormInput({ ...formInput, garmentType: e.target.value })}>
-              <option value="choose">Choose One</option>
-              <option value="shirt">Shirts</option>
-              <option value="blouse">Blouses</option>
-              <option value="shorts">Shorts</option>
-              <option value="leggings">Leggings</option>
-              <option value="trousers">Trousers</option>
-              <option value="skirts">Skirts</option>
-              <option value="pants">Pants</option>
-              <option value="jackets">Jackets</option>
-            </select>
-          </label>
-          <label>
-            Weather Compatibility:
-            <select 
-             name="Weather Compatability"
-             value={formInput.weatherCompatibility}
-             onChange={(e) => setFormInput({ ...formInput, weatherCompatibility: e.target.value })}>
-             <option value="choose">Please Choose One</option>
-             <option value="fall">Fall</option>
-             <option value="spring">Spring</option>
-             <option value="summer">Summer</option>
-             <option value="winter">Winter</option>
-            </select>
-          </label>
-          <label>
-            Style Type:
-            <select 
-                name="Style Type"
-                value={formInput.styleType}
-                onChange={(e) => setFormInput({ ...formInput, styleType: e.target.value })}>
-                <option value="choose">Please Choose One</option>
-                <option value="athleisure">Athleisure</option>
-                <option value="business">Business</option>
-                <option value="casual">Casual</option>
-                <option value="chic">Chic</option>
-                <option value="elegance">Elegance</option>
-                <option value="grunge">Grunge</option>
-                <option value="sporty">Sporty</option>
-                <option value="streetwear">Streetwear</option>
-            </select>
-          </label>
-          <label>
-            Color:
-            <select 
-                name="Color"
-                value={formInput.color}
-                onChange={(e) => setFormInput({ ...formInput, color: e.target.value })}>
-                <option value="choose">Please Choose One</option>
-                <option value="tan">Tan</option>
-                <option value="gray">Gray</option>
-                <option value="blue">Blue</option>
-                <option value="light-gray">Light-Gray</option>
-                <option value="black">Black</option>
-                <option value="white">White</option>
-                <option value="green">Green</option>
-                <option value="pink">Pink</option>
-                <option value="red">Red</option>
-            </select>
-          </label>
-          <label>
-            Occasion:
-            <select 
-                name="Occasion"
-                value={formInput.occasion}
-                onChange={(e) => setFormInput({ ...formInput, occasion: e.target.value })}>
-                <option value="choose">Please Choose One</option>
-                <option value="business">Business</option>
-                <option value="casual">Casual</option>
-                <option value="formal">Formal</option>
-                <option value="athletic">Athletic</option>
-            </select>
-          </label>
-          <label>
-            Gender:
-                <input type="text"
-                name="Gender"
-                value={formInput.gender}
-                onChange={(e) => setFormInput({ ...formInput, gender: e.target.value })}/>
-          </label>
-          <br />
-          <button type="submit">Upload</button>
-    </form>
-  );
-}
->>>>>>> 3b391c9 (Added random outfit + unified some styling)
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -153,13 +32,13 @@ const GarmentUploadForm = () => {
 
       try {
         const result = await response.json();
-        setResponseMessage(result.ok ? 'Upload successful!' : result.error);
+        setResponseMessage(result.ok ? "Upload successful!" : result.error);
       } catch (error) {
-        setResponseMessage('Non-JSON response received.');
+        setResponseMessage("Non-JSON response received.");
       }
     } catch (error) {
       setResponseMessage(`Error during upload: ${error.message}`);
-      console.error('Error during upload:', error);
+      console.error("Error during upload:", error);
     }
   };
 
@@ -170,13 +49,14 @@ const GarmentUploadForm = () => {
   return (
     <div>
       <h2>Garment Upload Form</h2>
-      <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
-      <label>
+      <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+        <label>
           Garment Type:
           <select
             name="Garment Type"
             value={formInput.garment_type}
-            onChange={(e) => handleInputChange("garment_type", e.target.value)}>
+            onChange={(e) => handleInputChange("garment_type", e.target.value)}
+          >
             <option value="choose">Choose One</option>
             <option value="shirt">Shirts</option>
             <option value="blouse">Blouses</option>
@@ -193,7 +73,10 @@ const GarmentUploadForm = () => {
           <select
             name="Weather Compatability"
             value={formInput.weather_compatability}
-            onChange={(e) => handleInputChange("weather_compatability", e.target.value)}>
+            onChange={(e) =>
+              handleInputChange("weather_compatability", e.target.value)
+            }
+          >
             <option value="choose">Please Choose One</option>
             <option value="fall">Fall</option>
             <option value="spring">Spring</option>
@@ -206,7 +89,8 @@ const GarmentUploadForm = () => {
           <select
             name="Style Type"
             value={formInput.style_type}
-            onChange={(e) => handleInputChange("style_type", e.target.value)}>
+            onChange={(e) => handleInputChange("style_type", e.target.value)}
+          >
             <option value="choose">Please Choose One</option>
             <option value="athleisure">Athleisure</option>
             <option value="business">Business</option>
@@ -223,7 +107,8 @@ const GarmentUploadForm = () => {
           <select
             name="Color"
             value={formInput.color}
-            onChange={(e) => handleInputChange("color", e.target.value)}>
+            onChange={(e) => handleInputChange("color", e.target.value)}
+          >
             <option value="choose">Please Choose One</option>
             <option value="tan">Tan</option>
             <option value="gray">Gray</option>
@@ -241,7 +126,8 @@ const GarmentUploadForm = () => {
           <select
             name="Occasion"
             value={formInput.occasion}
-            onChange={(e) => handleInputChange("occasion", e.target.value)}>
+            onChange={(e) => handleInputChange("occasion", e.target.value)}
+          >
             <option value="choose">Please Choose One</option>
             <option value="business">Business</option>
             <option value="casual">Casual</option>
@@ -255,7 +141,8 @@ const GarmentUploadForm = () => {
             type="text"
             name="Gender"
             value={formInput.forMen}
-            onChange={(e) => handleInputChange("forMen", e.target.value)} />
+            onChange={(e) => handleInputChange("forMen", e.target.value)}
+          />
         </label>
         <label>
           Image URL:
@@ -263,14 +150,16 @@ const GarmentUploadForm = () => {
             type="text"
             name="img_url"
             value={formInput.img_url}
-            onChange={(e) => handleInputChange("img_url", e.target.value)} />
+            onChange={(e) => handleInputChange("img_url", e.target.value)}
+          />
         </label>
         <label>
           Description:
           <textarea
             name="description"
             value={formInput.description}
-            onChange={(e) => handleInputChange("description", e.target.value)} />
+            onChange={(e) => handleInputChange("description", e.target.value)}
+          />
         </label>
         <br />
         <button type="submit">Upload</button>
